363af88090e29ac721c43f66fba4d2f2e83f35d3
CezarCobuz <cobuzcezar@gmail.com>
2020-07-21 23:15:32 +0300

    Updates

---
+++ precache-manifest.a2aeaef48e85b7e2a6ad76346edb6d0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1419feb817d04cfbea35452effb57d88",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "2edb53b039d4684fc52b",
+    "url": "/tic-tac-toe/static/css/main.f56818af.chunk.css"
+  },
+  {
+    "revision": "bca1d545be3ae5fc0012",
+    "url": "/tic-tac-toe/static/js/2.def9927f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.def9927f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2edb53b039d4684fc52b",
+    "url": "/tic-tac-toe/static/js/main.0d787698.chunk.js"
+  },
+  {
+    "revision": "1ce3fc15d72e4524b287",
+    "url": "/tic-tac-toe/static/js/runtime-main.4d5e2f67.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/tic-tac-toe/static/media/logo.ee7cd8ed.svg"
+  }
+]);
