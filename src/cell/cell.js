5216d22b51ced5d08baf5d7ea9c7887c700300ba
Cezar Cobuz <cezar.cobuz@zentahub.com>
2020-07-24 14:38:33 +0300

    Minor - vmin

--- src/cell/cell.js
+++ src/cell/cell.js
@@ -9,19 +9,19 @@ export const CellComponent = ({
   onPress = () => {},
   valueColor = "default",
 }) => {
-  const { height, width } = useWindowDimensions()
-
   // TODO: Apply responsive window on parent and all flex: 1 here
   return (
     <div
       key={value}
-      className={`wrapper ${height < width ? "wrapperByHeight" : null} ${
-        value !== 0 ? null : "wrapperHovered"
-      }`}
+      className={`wrapper ${value !== 0 ? null : "wrapperHovered"}`}
       onClick={value === 0 ? () => onPress() : () => {}}
     >
       {value !== 0 && (
-        <p className={`symbol ${valueColor === 'contrast'? "symbolContrast" : ""}`}>
+        <p
+          className={`symbol ${
+            valueColor === "contrast" ? "symbolContrast" : ""
+          }`}
+        >
           {value}
         </p>
       )}
